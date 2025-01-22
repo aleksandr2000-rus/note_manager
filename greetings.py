760a8c5d7cbe110f6360b1b1265402b0be2d9ea4
aleksandr2000-rus <aleksandrkuznecov46817@gmail.com>
2025-01-23 00:27:01 +0300

    greetings.py

--- greetings.py
+++ greetings.py
@@ -1,7 +1,12 @@
 username="Aleksandr"
+print("Aleksandr:", username)
 title="Project execution"
+print("Project execution:", title)
 content="Project execution in python"
+print("Project execution in python:", content)
 status="In process"
+print("In process:", status)
 created_date="21-01-2025"
+print("21-01-2025:", created_date)
 issue_date="21-03-2025"
-
+print("21-03-2025:", issue_date)
